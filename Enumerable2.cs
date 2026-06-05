c35bcbfeb5a3847917e4f1a86fb66e51392eff28
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-05 16:43:22 -0600

    added classes Enumerable2<T> and Enumerator2<T>

---
+++ Enumerable2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft.Collections
+{
+	public sealed class Enumerable2<T> : IEnumerable<T>
+	{
+		private IEnumerable<IEnumerable<T>> valuess;
+
+		public Enumerable2()
+		{
+		}
+
+		public Enumerable2(IEnumerable<IEnumerable<T>> valuess)
+		{
+			this.valuess = valuess;
+		}
+
+		public IEnumerable<IEnumerable<T>> Valuess { get => valuess; set => valuess = value; }
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			var value = new Enumerator2<T>(valuess);
+			return value;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ Enumerator2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Innovoft.Collections
+{
+	public sealed class Enumerator2<T> : IEnumerator<T>
+	{
+		public IEnumerator<IEnumerable<T>> valuess;
+		private IEnumerator<T> values;
+
+		public Enumerator2(IEnumerable<IEnumerable<T>> valuess)
+		{
+			this.valuess = valuess.GetEnumerator();
+			this.values = this.valuess.MoveNext() ? this.valuess.Current.GetEnumerator() : null;
+		}
+
+		public Enumerator2(IEnumerator<IEnumerable<T>> valuess)
+		{
+			this.valuess = valuess;
+			this.values = this.valuess.MoveNext() ? this.valuess.Current.GetEnumerator() : null;
+		}
+
+		~Enumerator2()
+		{
+			Dispose(disposing: false);
+		}
+
+		public T Current => values.Current;
+
+		object IEnumerator.Current => Current;
+
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+		}
+
+		private void Dispose(bool disposing)
+		{
+			if (!disposing)
+			{
+				return;
+			}
+
+			Interlocked.Exchange(ref values, null)?.Dispose();
+			Interlocked.Exchange(ref valuess, null)?.Dispose();
+		}
+
+		public bool MoveNext()
+		{
+			if (values == null)
+			{
+				return false;
+			}
+			while (true)
+			{
+				if (values.MoveNext())
+				{
+					return true;
+				}
+				if (!valuess.MoveNext())
+				{
+					return false;
+				}
+				values.Dispose();
+				values = valuess.Current.GetEnumerator();
+				continue;
+			}
+		}
+
+		public void Reset()
+		{
+			values?.Dispose();
+			valuess.Reset();
+			values = valuess.MoveNext() ? valuess.Current.GetEnumerator() : null;
+		}
+
+		public void Reset(IEnumerable<IEnumerable<T>> valuess)
+		{
+			this.values?.Dispose();
+			this.valuess.Dispose();
+			this.valuess = valuess.GetEnumerator();
+			this.values = this.valuess.MoveNext() ? this.valuess.Current.GetEnumerator() : null;
+		}
+
+		public void Reset(IEnumerator<IEnumerable<T>> valuess)
+		{
+			this.values?.Dispose();
+			this.valuess.Dispose();
+			this.valuess = valuess;
+			this.valuess.Reset();
+			this.values = this.valuess.MoveNext() ? this.valuess.Current.GetEnumerator() : null;
+		}
+	}
+}
